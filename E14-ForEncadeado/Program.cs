1f45713b06d2c22297d5e775ea04ce4b48832ee7
GILBERTO JUNIOR DOS SANTOS <giljrsantos@gmail.com>
2021-10-08 06:59:13 -0300

    A palavra chave BREAK

--- E14-ForEncadeado/Program.cs
+++ E14-ForEncadeado/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// PALAVRA CHAVE BREAK
 namespace E14_ForEncadeado
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Execuando projeto 14 - Palavra chave BREAK");
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -67,6 +67,10 @@
 <img src="./assets/img/screen-13.png" width="1800" alt="Dashboard">
 </p>
 
+## A PALAVRA CHAVE BREAK
+<p align="center">
+<img src="./assets/img/screen-14.png" width="1800" alt="Dashboard">
+</p>
 
 <!-- # ![Parabéns!](https://raw.githubusercontent.com/devsuperior/bds-assets/main/img/trophy.png)CONCLUÍDO -->
 
